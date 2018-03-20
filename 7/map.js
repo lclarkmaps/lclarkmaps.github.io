bf005bcdcffd4314e76083dffa46da8930f06a56
lclarkmaps <37426939+lclarkmaps@users.noreply.github.com>
2018-03-20 16:35:44 -0700

    Update map.js

--- 7/map.js
+++ 7/map.js
@@ -10,10 +10,10 @@ if (!mapboxgl.supported()) alert("Your browser does not support Mapbox GL");
 mapboxgl.accessToken = "pk.eyJ1IjoicHJvZmhvdXNlIiwiYSI6ImNqZXJnbmxkejE5ZHQycWx0NnF3Z2Z4djgifQ.cs5fxvvr9SxexUawshj4uw";
 map = new mapboxgl.Map({
     container: "map",
-    // style: 'mapbox://styles/mapbox/satellite-streets-v9',
+    style: 'mapbox://styles/mapbox/satellite-streets-v9',
     // style: "https://openmaptiles.github.io/dark-matter-gl-style/style-cdn.json",
     // style: "https://openmaptiles.github.io/positron-gl-style/style-cdn.json",
-    style: "https://openmaptiles.github.io/osm-bright-gl-style/style-cdn.json",            
+    // style: "https://openmaptiles.github.io/osm-bright-gl-style/style-cdn.json",            
     // style: "https://openmaptiles.github.io/klokantech-basic-gl-style/style-cdn.json",
     // style: "https://openmaptiles.github.io/klokantech-terrain-gl-style/style-cdn.json",            
     // style: "https://openmaptiles.github.io/klokantech-3d-gl-style/style-cdn.json",                        
